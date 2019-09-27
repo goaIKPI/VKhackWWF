b4391f352e0d8cab2c809d80993694a206776ee2
Azat <nevmetov.azat@gmail.com>
2019-09-27 18:35:29 +0300

    Initial commit. Added flask restapi boilerplate code from github

---
+++ backend/app/main/util/dto.py
@@ -0,0 +1,21 @@
+from flask_restplus import Namespace, fields
+
+
+class UserDto:
+    api = Namespace('user', description='user related operations')
+    user = api.model('user', {
+        'email': fields.String(required=True, description='user email address'),
+        'username': fields.String(required=True, description='user username'),
+        'password_hash': fields.String(required=True, description='user password'),
+        'public_id': fields.String(description='user Identifier'),
+        "registered_on": fields.DateTime(description="registered on")
+
+    })
+
+
+class AuthDto:
+    api = Namespace('auth', description='authentication related operations')
+    user_auth = api.model('auth_details', {
+        'email': fields.String(required=True, description='The email address'),
+        'password': fields.String(required=True, description='The user password '),
+    })
