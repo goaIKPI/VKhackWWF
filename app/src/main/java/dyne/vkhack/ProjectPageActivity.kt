478a1293b91909934062b7a1074edc605f4a3547
Bioman1464 <danilgap1464@gmail.com>
2019-09-28 16:23:17 +0300

    Stage 1.1

---
+++ app/src/main/java/dyne/vkhack/ProjectPageActivity.kt
@@ -0,0 +1,12 @@
+package dyne.vkhack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProjectPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fund_page_activity)
+    }
+}
